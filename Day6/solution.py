2675692f4317d2761e78f36972bc69b0fb769d0d
james-2001 <james.a.may.2001@gmail.com>
2021-08-07 20:10:02 +0100

    day6

--- Day6/solution.py
+++ Day6/solution.py
@@ -1,8 +1,23 @@
 f = open('input.txt', 'r')
-ls = [line.replace('\n','') for line in f.read().split('\n\n')]
+ls = [line for line in f.read().split('\n\n')]
 
-sum = 0
-for group in ls:
-    sum += len(set(group))
+p1_ls = [line.replace('\n', '') for line in ls]
 
-print(sum)
+count = 0
+for group in p1_ls:
+    count += len(set(group))
+
+print(f'Part 1: {count}')
+
+p2_ls = [line.split('\n') for line in ls]
+p2_ls = [list(filter(bool, line)) for line in p2_ls]
+
+p2_count = 0
+for group in p2_ls:
+    shared = group[0]
+    for person in group[1:]:
+        shared = ''.join(set(person).intersection(shared))
+    if len(shared) != 0:
+        p2_count += len(shared)
+
+print(f'Part 2: {p2_count}')
