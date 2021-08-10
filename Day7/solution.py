41298f3cd2a934308266f96a1b87c59b487babbc
james-2001 <james.a.may.2001@gmail.com>
2021-08-10 20:28:07 +0100

    day 7

--- Day7/solution.py
+++ Day7/solution.py
@@ -9,18 +9,32 @@ ls = [list(map(lambda s: s.replace(' contain', '').strip(',. '), line))
 bags = {line[0]: {bag[2:]: bag[0] for bag in line[1:]} for line in ls}
 
 
+class Node:
+    def __init__(self, colour, bags_dict) -> None:
+        self.next: list = []
+        self.prev: list = []
+        self.colour: str = colour
+        self.bags_dict: dict = bags_dict
+    
+    def find_prev(self):
+        for key, value in self.bags_dict.items():
+            if self.colour in value.keys():
+                self.prev.append(key)
 
 
-def contains(colour):
-      contain_set = {colour}
-      for key, value in bags.items():
-            inters = contain_set.intersection(set(value.keys()))
-            if inters:
-                  contain_set.add(key)              
-      return contain_set
-
-# if 'shiny gold' not in list(bags['shiny gold'].keys()):
-#       contain_set.remove('shiny gold')
+class Graph:
+    def __init__(self) -> None:
+        self.nodes: list = []
 
 
-# print((contain_set))
+graph = Graph()
+
+for colour, value in bags.items():
+    colour_node = Node(colour, bags)
+    colour_node.next = list(value.keys())
+    colour_node.find_prev()
+    graph.nodes.append(colour_node.colour)
+
+for colour in graph.nodes:
+    
+
