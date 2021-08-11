c978904eb30681224bded3d3bb99cc1b164860d8
james-2001 <james.a.may.2001@gmail.com>
2021-08-11 20:46:58 +0100

    day7 part 2

--- Day7/solution.py
+++ Day7/solution.py
@@ -1,19 +1,20 @@
 f = open('input.txt', 'r')
-ls = [line.replace('bags', 'bag').split('bag')
+ls = [line.replace('no', '0').replace('bags', 'bag').split('bag')
       for line in f.read().splitlines()]
 ls = [list(filter(lambda s: len(s) != 1, line)) for line in ls]
 ls = [list(map(lambda s: s.replace(' contain', '').strip(',. '), line))
       for line in ls]
 
-
-
-
-c_in = {line[0]: {bag[2:] for bag in line[1:]} for line in ls}
-c_by ={line[0]: {key for key, value in c_in.items() if line[0] in value} for line in ls}
-print(c_by)
+c_in = {line[0]: {(bag[2:], bag[0]) for bag in line[1:]} for line in ls}
+c_by = {line[0]: {key for key, value in c_in.items()
+        if line[0] in [pair[0] for pair in value]}
+        for line in ls}
+print(c_in)
 
 
 contains_gold = set()
+total_cost = 0
+
 
 def check(colour):
     for cl in c_by[colour]:
@@ -21,5 +22,16 @@ def check(colour):
         check(cl)
 
 
+def cost(colour, count):
+    global total_cost
+    if colour == 'other':
+        return
+    for cl in c_in[colour]:
+        total_cost += count*int(cl[1])
+        cost(cl[0], count*int(cl[1]))
+
+
 check('shiny gold')
-print(len(contains_gold))
+print(cost('shiny gold', 1))
+print(f'part 1: {len(contains_gold)}')
+print(f'part 2: {total_cost}')
