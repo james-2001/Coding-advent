e9c29b922b67a0bba26aa8cf835bb8f506b3c31a
james-2001 <james.a.may.2001@gmail.com>
2021-08-16 16:54:33 +0100

    day17

--- Day17/solution.py
+++ Day17/solution.py
@@ -1,14 +1,26 @@
 import numpy as np
+from scipy import ndimage as sp
 
 f = open('input.txt', 'r')
-ls = [line.split() for line in f.read().splitlines()]
-ls = np.array([list(line[0]) for line in ls])
+ls = [list(line.replace('#', '1').replace('.', '0'))
+      for line in f.read().splitlines()]
+ls = np.array([list(map(int, line)) for line in ls])
 
 
+def cycles(state, dim, n):
+    state = np.expand_dims(state, axis=tuple(range(dim-2)))
 
-def iterate(start, n):
-    while n > 0:
-        new = start.append
-        new =[]
-        iterate(new, n-1)
-    return
+    convolve_array = np.ones((3,)*dim)
+    convolve_array[(1,)*dim] = 0
+
+    for _ in range(n):
+        state = np.pad(state, 1).astype(int)
+        neigbour_count = sp.convolve(state, convolve_array,
+                                     mode='constant', cval=0.0)
+        state = (((state == 1) & ((neigbour_count == 3) | (neigbour_count == 2)))
+                 | ((state == 0) & (neigbour_count == 3)))
+    return np.sum(state)
+
+
+print(f'Part 1: {cycles(ls,3,6)}')
+print(f'Part 2: {cycles(ls,4,6)}')
