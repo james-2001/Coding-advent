71493f5318cdda042d9a1c32e539554708eff5c5
james-2001 <james.a.may.2001@gmail.com>
2021-08-17 22:02:28 +0100

    15 1&2

---
+++ Day15/solution.py
@@ -0,0 +1,9 @@
+def play(n):
+    spoken = [8, 13, 1, 0, 18, 9]
+    prev = spoken[-1]
+    history = {number: turn + 1 for turn, number in enumerate(spoken)}
+    for i in range(6, n):
+        history[prev], prev = i, i - history[prev] if prev in history else 0
+
+print(f'Part 1: {play(2020)}')
+print(f'Part 2: {play(30000000)}')
