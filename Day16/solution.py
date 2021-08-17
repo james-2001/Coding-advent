cd95c4f1e8f034c7691cfe4e0f60d8793e211b2c
james-2001 <james.a.may.2001@gmail.com>
2021-08-17 20:19:46 +0100

    16 1&2

--- Day16/solution.py
+++ Day16/solution.py
@@ -1,4 +1,7 @@
+import numpy as np
 import itertools as it
+import math
+from numpy.lib.shape_base import column_stack
 
 f = open('input.txt', 'r')
 fields, ticket, nearby = [line.split("\n") for line in f.read().split("\n\n")]
@@ -17,9 +20,40 @@ valid_set = set()
 for ranges in fields.values():
     range_0 = list(map(int, ranges[0]))
     range_1 = list(map(int, ranges[1]))
-    valid_set = valid_set.union({i for i in range(range_0[0], range_0[1]+1)})
-    valid_set = valid_set.union({i for i in range(range_1[0], range_1[1]+1)})
+    valid_set = valid_set.union(set(range(range_0[0], range_0[1]+1)))
+    valid_set = valid_set.union(set(range(range_1[0], range_1[1]+1)))
 
 all_nearby = list(it.chain(*nearby))
 tser = sum(filter(lambda s: s not in valid_set, all_nearby))
 print(f'Part 1: {tser}')
+
+correct_nearby = np.array(list(filter(lambda s: all([i in valid_set for i in s]),
+                          nearby)))
+correct_nearby = np.insert(correct_nearby, 0, ticket, axis=0)
+correct_nearby = correct_nearby.transpose()
+
+field_ticket = {i: set() for i in fields.keys()}
+
+for column in correct_nearby:
+    for key, (range1, range2) in fields.items():
+        range1 = tuple(map(int, range1))
+        range2 = tuple(map(int, range2))
+        possible = set(range(range1[0], range1[1]+1)).union(set(range(range2[0], range2[1]+1)))
+        if set(column).issubset(possible):
+            field_ticket[key].add(column[0])
+
+sorted_fields = sorted(field_ticket, key= lambda i: len(field_ticket[i]))
+acc, used = [], set()
+
+for field in sorted_fields:
+    if field.startswith('departure'):
+        acc.append((field_ticket[field]-used).pop())
+    used.update(field_ticket[field])
+
+print(acc)
+
+"""
+This drove me up the wall
+- Needs linting and neatening
+- Python wont compute the product, but will if you hard code values and product of that list
+"""
