c18252424f8c4cbd3b63e5d54388307128fbda4c
james-2001 <james.a.may.2001@gmail.com>
2021-08-24 10:29:14 +0100

    hs 7.1

--- Day7/solution.hs
+++ Day7/solution.hs
@@ -1,18 +1,15 @@
 import Data.List (isInfixOf)
-import Data.Map (Map)
-import Data.Graph (Graph, Vertex, graphFromEdges, graphFromEdges', transposeG, dfs)
+import Data.Graph (graphFromEdges, transposeG, reachable)
 import Data.List.Split
-import Text.Read
-import Data.Maybe (isNothing)
+import Data.Maybe (fromMaybe)
 import GHC.Unicode (isDigit, isSpace)
 
 main = do
-    input <- readFile "test.txt"
-    let sLines = map (removeWhiteSpace . splitOn "  " . removeNumbers . removeWordContaining "bag" . removeWordContaining "contain") $ splitOn "\n" input
-    let (g, v, k) = graphFromEdges ([(head line, head line, tail line) | line <- sLines])
-    let gT = transposeG g
-    print $ v 3
-    print $ length $ dfs gT [7] 
+    input <- readFile "input.txt"
+    let sLines = parseInput input
+    let (g, _, k) = graphFromEdges ([(head line, head line, tail line) | line <- sLines])
+    print $ length (reachable (transposeG g) (fromMaybe 0 $ k "shinygold")) -1
+        
 
 removeWordContaining:: String -> String -> String  
 removeWordContaining target s = unwords $ map (\n -> if target `isInfixOf` n then "" else n ) $ words s
@@ -20,9 +17,8 @@ removeWordContaining target s = unwords $ map (\n -> if target `isInfixOf` n the
 removeNumbers:: String -> String 
 removeNumbers = filter (not. isDigit) 
 
-pairList:: [String] -> [String]
-pairList [] = []
-pairList (x:y:xs) = (x++y): pairList xs
-
 removeWhiteSpace:: [String] -> [String] 
-removeWhiteSpace  = map $ filter $ not . isSpace  
+removeWhiteSpace  = map $ filter $ not . isSpace
+
+parseInput:: String -> [[String]]
+parseInput s = map (removeWhiteSpace . splitOn "  " . removeNumbers . removeWordContaining "bag" . removeWordContaining "contain") $ splitOn "\n" s
