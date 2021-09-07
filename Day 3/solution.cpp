b6d31021a70204466b969711ef8b3a835f28a6c7
james-2001 <james.a.may.2001@gmail.com>
2021-09-07 15:39:36 +0100

    3 cpp

--- Day 3/solution.cpp
+++ Day 3/solution.cpp
@@ -5,21 +5,39 @@
 
 using namespace std;
 
-void main(){
+int tree_count(int slope[2]){
+    int slope_x = slope[0];
+    int slope_y = slope[1];
     string line;
     vector<string> map;
     int x = 0;
+    int y = 0;
     int trees = 0;
     ifstream input ("input.txt");
     if (input.is_open()){
         while (getline(input,line)){
             int width = line.size();
-            if (line[x] == '#'){
-                trees++;
+            if (y%slope_y == 0){
+                if (line[x] == '#'){
+                    trees++;
+                }
+                x=(x+slope_x)%width;
             }
-            x=(x+3)%width;
+            y++;
         }
         input.close();
     }
-    cout << trees;
+    return trees;
+}
+
+
+void main(){
+    int p1[2] = {3,1};
+    int p2[5][2] = {{1,1},{3,1},{5,1},{7,1},{1,2}};
+    int acc = 1;
+    for (int i=0; i<5; i++){
+        acc *= tree_count(p2[i]);
+    }
+    cout << tree_count(p1) << endl;
+    cout << acc << endl;
 }
