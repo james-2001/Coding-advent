ea639b46015ac14bf110450744b35dd8b4f1a55f
james-2001 <james.a.may.2001@gmail.com>
2021-09-13 11:50:24 +0100

    pt1 rewritten

--- Day 23/solution.py
+++ Day 23/solution.py
@@ -5,20 +5,29 @@ test = "389125467"
 test2 = "328915467"
 
 cups = deque(map(int,list(inpot)))
+cups.rotate(-1)
 current_index = 0
-def simulate(cups,n):
+
+def rotate_past(deq: deque, element):
+    while deq[-1]!= element:
+        deq.rotate(-1)
+
+
+def simulate(cups: deque,n):
     for _ in range(n):
-        destination = (cups[current_index]-2)%9 +1
-        cups.rotate(-1)
+        current = cups[-1]
+        destination = (current-2)%9 +1
         pickup = [cups.popleft() for _ in range(3)]
         pickup.reverse()
         while destination in pickup:
             destination = (destination-2)%9 + 1
-        destination_index = cups.index(destination)
-        for c in pickup:
-            cups.insert(destination_index+1, c)
+        rotate_past(cups, destination) 
+        cups.extendleft(pickup)
+        rotate_past(cups, current)
+        cups.rotate(-1)
     return cups
-    
+
+
 def pt1_output(cups):
     while cups[-1]!= 1:
         cups.rotate()
@@ -33,10 +42,10 @@ def pt2_output(cups):
             return([cups[i+1], cups[i+2]])
 
 print(pt1_output(simulate(cups,100)))
-cups = deque(map(int,list(inpot))) + deque(range(10, 10000000))
-x= simulate(cups, 10000000)
-print("nearly there")
-print(pt2_output(x))
+# cups = deque(map(int,list(inpot))) + deque(range(10, 10000000))
+# x= simulate(cups, 10000000)
+# print("nearly there")
+# print(pt2_output(x))
 
 
 
