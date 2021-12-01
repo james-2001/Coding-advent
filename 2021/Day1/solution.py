4293e8863e70430b43457121b5e6e45fa7137807
james-2001 <james.a.may.2001@gmail.com>
2021-12-01 11:06:00 +0000

    day 1

---
+++ 2021/Day1/solution.py
@@ -0,0 +1,8 @@
+with open("2021/Day1/input.txt") as f:
+    depths = list(map(int, f.read().splitlines()))
+
+count1 = [depths[i-1]<depths[i] for i in range(1,len(depths))]
+count2 = [sum(depths[i:i+3]) < sum(depths[i+1:i+4]) for i in range(len(depths))] 
+
+
+print(sum(count1), sum(count2))
